831cdd49e92c51107d4e7d8bfa44f8bf00154342
Hanna Lupico <hlupico@gmail.com>
2015-07-18 15:07:29 -0500

    Photo thumbnail & timestamp displayed in listView

--- app/src/main/java/co/hannalupi/photoapp/MainActivity.java
+++ app/src/main/java/co/hannalupi/photoapp/MainActivity.java
@@ -1,49 +1,68 @@
 package co.hannalupi.photoapp;
 
+import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     static final String TAG = "PhotoApp";
+    private ArrayList<Photo> mPhotoList = new ArrayList<Photo>();
+
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     public static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int THUMB_DIM = 100;
     private Uri fileUri;
+    private PhotoAdapter mAdapter;
+    private static File mediaStorageDir;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+        // Create the adapter to convert the array to views
+        mAdapter = new PhotoAdapter(this, mPhotoList);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // Put divider between ToDoItems and FooterView
+        getListView().setFooterDividersEnabled(true);
+        Log.v(TAG, "FooterDividersEnabled");
+
+        // Inflate footerView for footer_view.xml file
+        TextView footerView = (TextView) getLayoutInflater().inflate(R.layout.footer_view, null);
+
+        // Add footerView to ListView
+        getListView().addFooterView(footerView);
+        Log.v(TAG, "FooterView Added");
+
+
+        //Attach Listener to FooterView
+        footerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //TODO - Implement OnClick().
                 Log.i(TAG, "Entered footerView.setOnClickListener()");
 
                 // create Intent to take a picture and return control to the calling application
-
-                //added "android.provider." --> was it needed? if so why?
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
                 fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
@@ -55,6 +74,9 @@ public class MainActivity extends ActionBarActivity {
                 startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         });
+
+        // Attach the adapter to a ListView
+        setListAdapter(mAdapter);
 
     }
 
@@ -75,9 +97,8 @@ public class MainActivity extends ActionBarActivity {
             if (resultCode == RESULT_OK) {
 
                 Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();
+                updatePhotoList();
 
-//                PhotoItem mPhotoItem = new PhotoItem(data);
-//                mAdapter.add(mPhotoItem);
             }else if (resultCode == RESULT_CANCELED) {
 
                 Toast.makeText(this, "User Cancelled Image Capture", Toast.LENGTH_LONG).show();
@@ -87,7 +108,6 @@ public class MainActivity extends ActionBarActivity {
         }
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -125,7 +145,7 @@ public class MainActivity extends ActionBarActivity {
 
         Environment.getExternalStorageState();
 
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+        mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "PhotoApp");
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
@@ -153,4 +173,64 @@ public class MainActivity extends ActionBarActivity {
 
         return mediaFile;
     }
+
+    //*******
+    //BELOW IS ADDED FROM GITHUB
+    //*******
+    //
+
+    private void getPhotos(List<Photo> photoList) {
+
+        if (photoList != null && mediaStorageDir.exists()) {
+
+            // For now we are starting from fresh and rebuilding the list.
+            photoList.clear();
+            Log.i(TAG, "Storage directory exists!!");
+            for (File file : mediaStorageDir.listFiles()) {
+
+                Date date = new Date(file.lastModified());
+                String time = date.toString();
+
+                photoList.add(
+                        new Photo(
+                                time
+                                , file.getAbsolutePath()
+                                , getPhotoThumbnail(file.getAbsolutePath())
+                        )
+                );
+            }
+
+        }
+    }
+
+    private Bitmap getPhotoThumbnail(String photoPath) {
+        Log.i(TAG, "Get Photo Thumbnails");
+        // Get the dimensions of the View
+        int targetW = THUMB_DIM;
+        int targetH = THUMB_DIM;
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        return BitmapFactory.decodeFile(photoPath, bmOptions);
+    }
+
+    private void updatePhotoList() {
+        getPhotos(mPhotoList);
+        mAdapter.notifyDataSetChanged();
+    }
+
+
 }
